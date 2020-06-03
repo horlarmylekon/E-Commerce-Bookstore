adfac7edc448bd575178355008db49b1528c993d
akindutire <akinsamuel33@gmail.com>
2020-06-03 22:19:38 +0100

    App induced props

---
+++ src/main/java/com/intellisense/ebookstorev1/store/prop/AppProp.java
@@ -0,0 +1,9 @@
+package com.intellisense.ebookstorev1.store.prop;
+
+//Basic POJO to grab data from app.properties end
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppProp {
+}
