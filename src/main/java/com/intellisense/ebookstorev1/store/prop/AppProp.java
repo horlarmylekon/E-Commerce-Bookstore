ecd65f7207da21751c8172c77463b2be3d19ee5e
akindutire <akinsamuel33@gmail.com>
2020-06-03 23:49:14 +0100

    developed and modified some loaded props

--- src/main/java/com/intellisense/ebookstorev1/store/prop/AppProp.java
+++ src/main/java/com/intellisense/ebookstorev1/store/prop/AppProp.java
@@ -2,8 +2,22 @@ package com.intellisense.ebookstorev1.store.prop;
 
 //Basic POJO to grab data from app.properties end
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppProp {
+
+    @Value("${com.cloudinary.cloud_name}")
+    public String CLOUDINARY_CLOUD_NAME;
+
+    @Value("${com.cloudinary.api_key}")
+    public String CLOUDINARY_API_KEY;
+
+    @Value("${com.cloudinary.api_secret}")
+    public String CLOUDINARY_API_SECRET;
+
+    @Value("${folder.book}")
+    public String CLOUDINARY_UPLOAD_PATH;
+
 }
